15934922f5e443e48eb66ac74995d3930ca977e3
bramantyois <ibrahim.bramantyo@gmail.com>
2021-06-19 21:15:01 +0700

    mcts test

--- agents/agent_mcts/mcts.py
+++ agents/agent_mcts/mcts.py
@@ -8,6 +8,7 @@ import math
 
 import abc
 
+
 class State:
     def __init__(self, board: np.ndarray):
         """
@@ -19,8 +20,7 @@ class State:
         self.n = 0
         self.board = board
 
-
-    def backpropagate(self)->None:
+    def backpropagate(self) -> None:
         """
         Backpropagation. Update the intrinsic parameters of the state.
         """
@@ -45,23 +45,38 @@ class State:
 
         return identical_child
 
-    def is_leaf_node(self)->bool:
+    @property
+    def is_leaf_node(self) -> bool:
         """
         checking if the state is a leaf node
         :return:
         """
-        if self.children == []:
+        if not self.children:
             return True
         else:
             return False
 
-class C4MCTS:
+    def get_num_n(self) -> int:
+        return self.n
+
+    def add_child(self, child_state):
+        self.children.append(child_state)
+
+
+class Connect4MCTS:
     """
     implementation of Monte-carlo tree search on connect 4
     """
-    def __init__(self):
+
+    def __init__(self, player: BoardPiece):
         self.root_node = []
-        self.player = NO_PLAYER
+        self.player = player
+        self.c = 2
+
+        if player == PLAYER1:
+            self.competing_player = PLAYER2
+        else:
+            self.competing_player = PLAYER1
 
     def rollout(self, state: State):
         """
@@ -73,49 +88,63 @@ class C4MCTS:
         cur_player = self.player
         score = 0
 
-        cur_game_state = check_end_state(cur_board, cur_player)
-
-        while cur_game_state == GameState.STILL_PLAYING:
+        while check_end_state(cur_board, cur_player) == GameState.STILL_PLAYING:
             while True:
                 action = np.random.choice(7)
                 if check_valid_action(cur_board, action):
                     break
 
-            cur_board = (cur_board, action, cur_player)
+            cur_board = apply_player_action(cur_board, action, cur_player)
 
             if cur_player == PLAYER1:
                 cur_player = PLAYER2
             else:
                 cur_player = PLAYER1
 
-            cur_game_state = check_end_state(cur_board, cur_player)
-
-        if cur_game_state == GameState.IS_WIN and cur_player == self.player:
+        end_game_state = check_end_state(cur_board, cur_player)
+        if end_game_state == GameState.IS_WIN and cur_player == self.player:
             score = 1  # agent winning the game
-        elif cur_game_state == GameState.IS_DRAW:
+        elif end_game_state == GameState.IS_DRAW:
             score = 0.5
 
         return score
 
-    def USB1(self, state: State):
-        pass
+    def expand(self, state: State):
+        actions = np.arange(7)
 
-    def run_iteration(self, iteration_num : int = 1):
+        board = state.board.copy()
+        for action in actions:
+            if check_valid_action(board, action):
+                new_board = apply_player_action(action, board, self.player, copy=True)
+
+                for action2 in actions:
+                    if check_valid_action(new_board, action2):
+                        new_board_2 = apply_player_action(action, new_board, self.competing_player, copy=True)
+                        new_child = State(new_board_2)
+                        state.add_child(new_child.copy())
+
+    def run_iteration(self, iteration_num: int = 1):
         cur_state = self.root_node
 
         for _ in range(iteration_num):
             while True:
 
-                if not cur_state.is_leaf_node():
-                    cur_state =
+                if cur_state.is_leaf_node():
+                    if cur_state.get_num_n() == 0:
+                        self.expand(cur_state)
+                        cur_state = cur_state.children[0]
+                        self.rollout(cur_state)
+                        break
+                    else:
+                        self.rollout(cur_state)
+                else:
+                    idx = -1
+                    ucb1 = -999
+                    for i, child in enumerate(cur_state.children):
+                        new_val = child.score + self.c * math.sqrt(cur_state.N / cur_state.n)  # check division by zero
 
+                        if new_val > ucb1:
+                            idx = i
+                            ucb1 = new_val
 
-
-
-
-
-
-    def simulate(self, board, action):
-        turn = self.player
-        while check_end_state(board, self.player) == GameState.STILL_PLAYING:
-            board = apply_player_action(board, action, self.player)
+                    cur_state = cur_state.children[idx]
---
+++ tests/test_mcts.py
@@ -0,0 +1,22 @@
+import numpy
+
+
+def test_backpropagation():
+    pass
+
+def test_state_find_child():
+    pass
+
+def test_state_leaf_node():
+    pass
+
+
+def test_mcts_rollout():
+    pass
+
+def test_mcts_expand():
+    pass
+
+def test_run_iteration():
+    pass
+
