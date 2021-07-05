bug on backpropate state.py

agents/agent_mcts/mcts.py
@@ -11,9 +11,6 @@ import time
 
 
 class Connect4MCTS:
-    """
-    implementation of Monte-carlo tree search on connect 4
-    """
     def __init__(
             self,
             expansion_rate: int = 1,
@@ -21,6 +18,7 @@ class Connect4MCTS:
             max_t: float = 2,
             max_iter: int = 10):
         """
+        Class of a Monte-Carlo tree search agent on  game of connect 4
 
         :param max_iter:
         :param expansion_rate:
@@ -35,6 +33,7 @@ class Connect4MCTS:
         self._player = NO_PLAYER
         self._past_player = NO_PLAYER
         self._competing_player = NO_PLAYER
+
         self._c = 2
 
     def set_player(self, player: BoardPiece):
@@ -99,7 +98,6 @@ class Connect4MCTS:
         actions_1 = np.arange(7)
         actions_2 = np.arange(7)
 
-        np.random.shuffle(actions_1)
         np.random.shuffle(actions_2)
 
         board = state.get_board()
@@ -125,7 +123,11 @@ class Connect4MCTS:
                     #     break
 
     def iterate(self):
+        if len(self._root_node.get_children()) == 0:
+            self.expand(self._root_node)
+
         cur_state = self._root_node
+
         while True:
             if cur_state.is_leaf_node():
                 if cur_state.get_n() == 0:
@@ -168,7 +170,7 @@ class Connect4MCTS:
                 self.iterate()
 
     def choose_action(self):
-        max_score = -999
+        max_score = -math.inf
         child_idx = -1
         for i, child in enumerate(self._root_node.get_children()):
             score = child.get_score()

agents/agent_mcts/state.py
@@ -3,16 +3,16 @@ import copy
 
 
 class State:
-    """
-
-    :param board: board representing the state current node
-    :type _children: a list containing all the children node
-    :type _n: number of trial performed for tree
-    :type _score: score value of the tree
-    :type _board: current state board
-    """
     def __init__(self, board: np.ndarray):
+        """
+        Class representing a node in a tree.
 
+        :param board: board representing the state current node
+        :type self._children: a list containing all the children node
+        :type self._n: number of trial performed for tree
+        :type self._score: score value of the tree
+        :type self._board: current state board
+        """
         self._children = []
         self._score = 0
         self._n = 0
@@ -22,15 +22,19 @@ class State:
         """
         Backpropagation. Update the intrinsic parameters of the state.
         """
-
         total_score = 0
         total_n = 0
-        for child in self._children:
-            child.backpropagate()
-            total_score += child.get_score()
-            total_n += child.get_n()
-        self._score += total_score
-        self._n += total_n
+        if len(self._children) == 0:
+            total_score = self._score
+            total_n = self._n
+        else:
+            for child in self._children:
+                child.backpropagate()
+                total_score += child.get_score()
+                total_n += child.get_n()
+
+        self._score = total_score
+        self._n = total_n
 
     def find_child(self, board: np.ndarray):
         """
@@ -57,24 +61,51 @@ class State:
             return False
 
     def get_n(self) -> int:
+        """
+        Getter function returning number of simulation
+        :return: number of elapsed simulation
+        """
         return self._n
 
     def get_score(self) -> float:
+        """
+        Getter function returning the score
+        :return: score
+        """
         return self._score
 
-    def set_n(self, n: int) -> None:
-        self._n = n
+    # def set_n(self, n: int) -> None:
+    #     self._n = n
 
-    def set_score(self, score) -> None:
+    def set_score(self, score: float) -> None:
+        """
+        Setting the score of the node. Automatically increment number of step n
+
+        :param score: simulation score
+        :return: None
+        """
         if self._n <= 0:
             self._score = score
             self._n = 1
 
-    def add_child(self, child_state):
+    def add_child(self, child_state) -> None:
+        """
+        adding child state to the node
+        :param child_state: child state, should be a State. Automatically deep copy the child_state
+
+        """
         self._children.append(copy.deepcopy(child_state))
 
-    def get_board(self):
+    def get_board(self) -> np.ndarray:
+        """
+        getter function to get the current board of the State
+        :return: current board
+        """
         return self._board.copy()
 
     def get_children(self):
+        """
+        getter function to get the children of the State
+        :return: reference to the children
+        """
         return self._children

tests/test_state.py
@@ -30,8 +30,18 @@ def test_backpropagation() -> None:
     parent_node.backpropagate()
 
     assert (parent_node.get_score() == np.sum(scores))
-    assert (parent_node.get_n() == 3)
+    # assert (parent_node.get_n() == 3)
 
+    score = np.random.choice([0, 0.5, 1])
+
+    child_node_13 = State(test_board)
+    child_node_13.set_score(score)
+    parent_node.add_child(child_node_13)
+
+    parent_node.backpropagate()
+
+    assert (parent_node.get_score() == np.sum(scores) + score)
+    # assert (parent_node.get_n() == 4)
 
 def test_state_find_child():
     test_board_1 = np.full((6, 7), NO_PLAYER)